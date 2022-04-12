d7a2d39175931b1fc52a28d6f9e9b2c873fe882a
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-12 15:18:48 -0700

    create a hash map

---
+++ 220412-Lonely-Integer/algo.js
@@ -0,0 +1,14 @@
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(a) {
+    const aHash = new Map();
+    console.log({aHash});
+
+}
+
+console.log("lonelyinteger[1,2,3,4,3,2,1]:", lonelyinteger( [1,2,3,4,3,2,1] ));
