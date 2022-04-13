5bd668ff21d2ae28c5cc52be90c53ffcb15c11ec
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-13 10:32:47 -0700

    add diagonalDifference function

--- 220413-Diagonal-Difference/algo.js
+++ 220413-Diagonal-Difference/algo.js
@@ -1,3 +1,15 @@
 /////////////////////////////////////////////
 //  220413 Diagonal Difference Algo.js
 /////////////////////////////////////////////
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference(arr) {
+    // Write your code here
+
+}
