1779ea29cf99a1685b3a46795e63be04a0b3b907
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-14 11:39:40 -0700

    add countingSort function

--- 220414-Counting-Sort/algo.js
+++ 220414-Counting-Sort/algo.js
@@ -1,3 +1,15 @@
 ///////////////////////////////////////////
 //  220414 Counting Sort Algo.js
 ///////////////////////////////////////////
+
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function countingSort(arr) {
+    // Write your code here
+
+}
