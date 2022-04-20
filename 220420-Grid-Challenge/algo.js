99514c5c13d56860fd98ee742ad93ec0c28ad9cf
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-20 16:46:37 -0700

    update

--- 220420-Grid-Challenge/algo.js
+++ 220420-Grid-Challenge/algo.js
@@ -18,14 +18,32 @@ function gridChallenge(grid) {
     // 5. Otherwise, return YES
     console.log({grid});
 
+    // Create a Character Grid
+    let charGrid=[];
+
     // Sort thr grids horizontally
     for (item of grid) {
-        console.log({item});
+        charGrid.push(item.split('').sort());
     }
+    console.log({charGrid});
+
+    // Go through the character grid vertically to see if they are sorted
+    for (let x=0; x<charGrid[0].length; x++) {      // x = horizontal position
+        let prevLetter = 'a';
+        for (let y=0; y<charGrid.length; y++) {     // y = vertical position
+            if (prevLetter > charGrid[x,y]) {
+                return "NO"
+            }
+            prevLetter = charGrid[x,y];
+        }
+    }
+    return "YES"
 }
 
 console.log("************************");
 console.log("Algo.js");
 console.log("************************\n");
 
-console.log(gridChallenge([ 'eabcd', 'fghij', 'olkmn', 'trpqs', 'xywuv' ]));
+console.log("gridChallenge([ 'eabcd', 'fghij', 'olkmn', 'trpqs', 'xywuv' ]):", gridChallenge([ 'eabcd', 'fghij', 'olkmn', 'trpqs', 'xywuv' ]));
+
+console.log("gridChallenge([ 'eabcd', 'olkmn', 'fghij', 'trpqs', 'xywuv' ]):", gridChallenge([ 'eabcd', 'olkmn', 'fghij', 'trpqs', 'xywuv' ]));
