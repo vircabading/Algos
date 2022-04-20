48550ae5a4b8af1f4223e3d7bcd271f89b87a092
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-20 15:58:19 -0700

    add gridChallenge function

--- 220420-Grid-Challenge/algo.js
+++ 220420-Grid-Challenge/algo.js
@@ -2,6 +2,18 @@
 //  220420 Grid Challenge algo.js
 /////////////////////////////////////
 
+/**
+ * Complete the 'gridChallenge' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING_ARRAY grid as parameter.
+ */
+
+function gridChallenge(grid) {
+    // Write your code here
+    console.log({grid});
+}
+
 console.log("************************");
 console.log("Algo.js");
 console.log("************************");
