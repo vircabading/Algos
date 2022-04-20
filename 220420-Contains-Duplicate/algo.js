4c1df2ebe82f696c32212d64f683777db70aced4
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-20 09:59:19 -0700

    add pseudo code

--- 220420-Contains-Duplicate/algo.js
+++ 220420-Contains-Duplicate/algo.js
@@ -8,6 +8,12 @@
  */
  var containsDuplicate = function(nums) {
     // Pseudo Code
+    // 1. Create a hashmap
+    // 2. Go through the nums array and see if each element is in the hash map
+    // 3. If the element is not in the hash map, add the element to the hash map
+    // 4. If the element is already in the map, it means duplicate is found
+    // 5. If duplicate is found return true
+    // 6. If no duplicate found in the array, return false
 };
 
 console.log("////////////////////////////");
