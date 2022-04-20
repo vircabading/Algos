ea43ce47ca7e5f264eb7a6da5756b1cfbe44f8e2
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-20 09:52:27 -0700

    add contiansDuplicate function

--- 220420-Contains-Duplicate/algo.js
+++ 220420-Contains-Duplicate/algo.js
@@ -1,3 +1,11 @@
 ////////////////////////////////////////////
 //  220420 Contains Duplicate algo.js
 ////////////////////////////////////////////
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ var containsDuplicate = function(nums) {
+    
+};
