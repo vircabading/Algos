70a664721f175f98b9bf93380420f5858963fd39
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-21 09:42:34 -0700

    implement

--- 220421-Missing-Number/algo.js
+++ 220421-Missing-Number/algo.js
@@ -12,9 +12,29 @@
     //      where n is the length of nums array
     // 2. Go through the nums array and remove each item found from the expected hash map
     // 3. In the end, the remaining number in the expected numbers map is the missing number
+
+    const n = nums.length;
+
+    // Create expected numbers hash map
+    const expectNums = new Map();
+    for (let x=0; x<=n; x++) {
+        expectNums[x] = x;
+    }
+
+    // Go through the nums array and remove each item found from expected nums hash map
+    for (let num of nums) {
+        expectNums[num] = -1;
+    }
+
+    for (num in expectNums) {
+        if (expectNums[num] >= 0) {
+            return expectNums[num]
+        }
+    }
 };
 
 console.log("******************************");
 console.log("Missing Number Algorithm")
 console.log("******************************\n");
 
+console.log("expectNums([3,0,1]):", missingNumber([3,0,1]));
