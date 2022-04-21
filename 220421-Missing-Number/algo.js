5cfc02ca0d7b6f8e205ea4e71bfb0109f5851655
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-21 09:03:31 -0700

    add missingNumber function

--- 220421-Missing-Number/algo.js
+++ 220421-Missing-Number/algo.js
@@ -2,6 +2,15 @@
 //  220421 Missing Number
 //////////////////////////////////////////////
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var missingNumber = function(nums) {
+    
+};
+
 console.log("******************************");
 console.log("Missing Number Algorithm")
 console.log("******************************\n");
+
