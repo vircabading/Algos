453afad60fbe2452d39810e805cea9bc81783003
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-21 09:07:16 -0700

    add pseudo code

--- 220421-Missing-Number/algo.js
+++ 220421-Missing-Number/algo.js
@@ -8,6 +8,10 @@
  */
  var missingNumber = function(nums) {
     // Pseudo Code
+    // 1. Create a hash map of expected numbers in the array [0,n]
+    //      where n is the length of nums array
+    // 2. Go through the nums array and remove each item found from the expected hash map
+    // 3. In the end, the remaining number in the expected numbers map is the missing number
 };
 
 console.log("******************************");
