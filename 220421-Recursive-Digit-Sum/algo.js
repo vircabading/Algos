cb72b09bfa996e9fcad7faa9caad32d647cfc680
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-21 11:48:01 -0700

    add pseudocode

--- 220421-Recursive-Digit-Sum/algo.js
+++ 220421-Recursive-Digit-Sum/algo.js
@@ -13,6 +13,15 @@
 
 function superDigit(n, k) {
     // Pseudo Code
+    // 1. Construct p by concatenating string n k times
+    // 2. Retrieve each digit from p
+    // 2.A Iterate through string p to create an array of single digit ints from p
+    // 3. Get a sum of all the digits
+    // This should be recursive
+    // A. Base Case
+    //      n length = 1 and k = 1, return n as a integer
+    // B. Recursive Case
+    //      Recursively call superDigit on the sum of all the digits of p
 
 }
 
