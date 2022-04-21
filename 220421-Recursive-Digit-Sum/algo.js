0e0e1e9932243dcac19acf853bbd6e29eef8e61d
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-21 11:38:56 -0700

    add superDigit Function

--- 220421-Recursive-Digit-Sum/algo.js
+++ 220421-Recursive-Digit-Sum/algo.js
@@ -2,6 +2,23 @@
 //  220421 Recursive Digit Sum algo.js
 ///////////////////////////////////////////////
 
+/**
+ * Complete the 'superDigit' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING n
+ *  2. INTEGER k
+ */
+
+function superDigit(n, k) {
+    // Pseudo Code
+
+}
+
+
 console.log("********************************");
 console.log("Recursive Digit Sum Algorithm");
 console.log("********************************\n");
+
+console.log("superDigit('148', 3):", superDigit('148', 3));
