f0fb11d7322f98799d4bcece5a8a8997511bee40
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-25 11:03:05 -0700

    add pseudocode to algo.js

--- 220425-New-Year-Chaos/algo.js
+++ 220425-New-Year-Chaos/algo.js
@@ -10,6 +10,15 @@
 
 function minimumBribes(q) {
     // Pseudo Code
+    // 1, Keep track of running totl # of bribes
+    // 2. Go through array q and get each element from the array
+    // 3. If the element from q is farther forward than expected,
+    //      determine how far ahead it is from it's expected position
+    //      and add that number to the running total # of bribes
+    // 4. If any element from q is more than 2 positions ahead of its
+    //      expected position, print "Too chaotic"
+    // 5. After having finished iterating through all elements in array q
+    //      print the total # of bribes
     console.log({q});
 }
 
