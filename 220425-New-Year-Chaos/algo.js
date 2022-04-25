2abd268406b5ba84d46d0caa49dd9a3cc1ea4627
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-25 10:54:05 -0700

    add minimum bribes function

--- 220425-New-Year-Chaos/algo.js
+++ 220425-New-Year-Chaos/algo.js
@@ -2,3 +2,13 @@
 //  220425 New Year Chaos Algorithm algo.js
 /////////////////////////////////////////////
 
+/*
+ * Complete the 'minimumBribes' function below.
+ *
+ * The function accepts INTEGER_ARRAY q as parameter.
+ */
+
+function minimumBribes(q) {
+    // Write your code here
+    console.log({q});
+}
