bf0b50faeba33377a8bc63d80c1d4aa70e21af49
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-26 12:39:56 -0700

    add pseudocde

--- 220426-Merge-Two-Sorted-Lists/algo.js
+++ 220426-Merge-Two-Sorted-Lists/algo.js
@@ -53,6 +53,19 @@ function printSinglyLinkedList(node, sep, name) {
  *
  */
 function mergeLists(head1, head2) {
+    // Pseudo Code
+    // 1. Create an Output linkList
+    // 2. Create 2 runners pointing to the heads of head1 and head2
+    // 3. Iterate through head1 and head2 until get to the end of one list
+    // 3.1 if data at runner 1 is < or = to data at runner 2
+    // 3.1.A    add data at runner 1 to Output Linked List
+    // 3.1.B    point runner 1 to the next node in the list
+    // 3.2 else (means data at runner 2 is < data at runner 1) 
+    // 3.2.A    therefore, add data at runner to to Output Linked List
+    // 3.2.B    point runner 2 to the next node in the list
+    // 4. Figure out which list is still not empty
+    // 5. Move the remaining data from the remaining non-empty list to the Output
+    // 6. Return the Output SinglyLinkedList
 
     console.log({head1});
     console.log({head2});
