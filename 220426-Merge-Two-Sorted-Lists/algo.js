f9594714b11f0d97bf1544ef67ecdc5cb38a5ace
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-26 11:44:47 -0700

    add Single Linked List Node and Class

--- 220426-Merge-Two-Sorted-Lists/algo.js
+++ 220426-Merge-Two-Sorted-Lists/algo.js
@@ -2,3 +2,41 @@
 //  220426 Merge Tso Sorted Linked Lists Algorithm algo.js
 ///////////////////////////////////////////////////////////
 
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
