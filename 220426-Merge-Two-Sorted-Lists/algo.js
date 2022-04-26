7b0add3ea9ec2a58f032233ef225a4d0bf5b02e2
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-26 12:49:18 -0700

    iterate through the lists until one is empty

--- 220426-Merge-Two-Sorted-Lists/algo.js
+++ 220426-Merge-Two-Sorted-Lists/algo.js
@@ -67,8 +67,32 @@ function mergeLists(head1, head2) {
     // 5. Move the remaining data from the remaining non-empty list to the Output
     // 6. Return the Output SinglyLinkedList
 
-    console.log({head1});
-    console.log({head2});
+    // 1. Create an Output linkList
+    // 2. Create 2 runners pointing to the heads of head1 and head2
+    let output = new SinglyLinkedList();
+    let runner1 = head1.head;
+    let runner2 = head2.head;
+
+    // 3. Iterate through head1 and head2 until get to the end of one list
+    while (runner1 && runner2) {
+        // 3.1 if data at runner 1 is < or = to data at runner 2
+        // 3.1.A    add data at runner 1 to Output Linked List
+        // 3.1.B    point runner 1 to the next node in the list
+        if (runner1.data <= runner2.data) {
+            output.insertNode(runner1.data);
+            runner1 = runner1.next;
+        }
+        // 3.2 else (means data at runner 2 is < data at runner 1) 
+        // 3.2.A    therefore, add data at runner to to Output Linked List
+        // 3.2.B    point runner 2 to the next node in the list
+        else {
+            output.insertNode(runner2.data);
+            runner2 = runner2.next;
+        }
+    }
+
+    printSinglyLinkedList(output, " --> ", "Output");
+    console.log({output});
 }
 
 console.log("*********************************");
