8ee1f2e2b0a927c6c5077cad0e765a4703c17caa
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-28 11:10:26 -0700

    add getPhoneNumbers to homeComp

--- 220428-Rest-API-Country-Codes/countrycodes/src/App.js
+++ 220428-Rest-API-Country-Codes/countrycodes/src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 
-import axios from 'axios';
-import React, {useEffect, useState} from 'react';
 
-const [countryCode, setCountryCode] = useState([]);
 
-let getPhoneNumbers = (country, phoneNumber) => {
-  console.log({country});
-  console.log({phoneNumber});
-  console.log({countryCode});
-};
 
-getPhoneNumbers("country", "number");
+
+
+
+
 
 function App() {
   return (
--- 220428-Rest-API-Country-Codes/countrycodes/src/components/homeComp.jsx
+++ 220428-Rest-API-Country-Codes/countrycodes/src/components/homeComp.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const homeComp = () => {
-  return (
-    <div>Home Component</div>
-  )
+    const [countryCode, setCountryCode] = useState([]);
+
+    const getPhoneNumbers = (country, phoneNumber) => {
+        console.log({ country });
+        console.log({ phoneNumber });
+        console.log({ countryCode });
+    };
+
+    getPhoneNumbers("country", "number");
+
+    return (
+        <div>Home Component</div>
+    )
 }
 
 export default homeComp
