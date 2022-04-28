fd3b3aad8d39d2468ebc12b6a1f372b9e2e4825e
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-28 11:07:25 -0700

    create home component

--- 220428-Rest-API-Country-Codes/countrycodes/src/App.js
+++ 220428-Rest-API-Country-Codes/countrycodes/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+
 import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+
+const [countryCode, setCountryCode] = useState([]);
 
 let getPhoneNumbers = (country, phoneNumber) => {
   console.log({country});
   console.log({phoneNumber});
+  console.log({countryCode});
 };
 
 getPhoneNumbers("country", "number");
---
+++ 220428-Rest-API-Country-Codes/countrycodes/src/components/homeComp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const homeComp = () => {
+  return (
+    <div>Home Component</div>
+  )
+}
+
+export default homeComp
