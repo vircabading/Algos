106a98d7a5570152298bf2879d2b322d130279ee
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-28 11:53:13 -0700

    execute useEffect

--- 220428-Rest-API-Country-Codes/countrycodes/src/components/homeComp.jsx
+++ 220428-Rest-API-Country-Codes/countrycodes/src/components/homeComp.jsx
@@ -1,19 +1,42 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const HomeComp = () => {
-    const [countryCode, setCountryCode] = useState([]);
+    let [countryCode, setCountryCode] = useState([]);
+    const [fullPhoneNumber, setfullPhoneNumber] = useState("")
 
     const getPhoneNumbers = (country, phoneNumber) => {
-        console.log({ country });
-        console.log({ phoneNumber });
-        console.log({ countryCode });
+        // Get info from API using country name
+        axios.get("https://jsonmock.hackerrank.com/api/countries?name=" + country)
+            .then(res => {
+                // Retrieve the array of calling Codes
+                let callingCodes = res.data.data[0].callingCodes
+                setCountryCode(callingCodes);
+
+
+                let thisCountryCode = -1;
+                // Get the highest indexed Country Code
+                if (callingCodes > 0) {
+                    thisCountryCode = callingCodes[callingCodes.length - 1];
+                }
+
+                // Format the final phone number
+                let output = "+" + thisCountryCode + " " + phoneNumber;
+                console.log({ output });
+                setfullPhoneNumber(output);
+            })
     };
 
-    getPhoneNumbers("country", "number");
+    useEffect(() => {
+        getPhoneNumbers("Vietnam", "123-4567");
+    }, []);
 
     return (
-        <div>Home Component</div>
+        <div>
+            <h2>Home Component</h2>
+            <p>Country Code: {countryCode}</p>
+            <p>Full Phone Number: {fullPhoneNumber}</p>
+        </div>
     )
 }
 
