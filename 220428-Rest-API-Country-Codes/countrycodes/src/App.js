a3f2c66b83c6c2a34afce8f5cafcc00e1daef557
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-28 10:55:46 -0700

    add function getPhoneNumbers

--- 220428-Rest-API-Country-Codes/countrycodes/src/App.js
+++ 220428-Rest-API-Country-Codes/countrycodes/src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+let getPhoneNumbers = (country, phoneNumber) => {
+  console.log("*** Testing ****");
+};
+
+getPhoneNumbers("country", "numbrer");
+
 function App() {
   return (
     <div className="App">
