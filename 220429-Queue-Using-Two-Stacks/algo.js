bf7b377a684306a574bc27290e7adae13979fcbe
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-29 12:24:15 -0700

    figure out which operation is up

--- 220429-Queue-Using-Two-Stacks/algo.js
+++ 220429-Queue-Using-Two-Stacks/algo.js
@@ -21,6 +21,20 @@ function processData(input) {
     }
     operations.push( parseInt(operation));
     console.log({operations});
+
+    // Iterate through the operations
+    for (let x = 1; x<operations.length; x++) {
+        operation = operations[x];
+        if (operation == 1) {
+            x++;
+            let number = operation[x];
+            console.log("Operation:", operation, "| action: enqueue", number);
+        } else if (operation == 2) {
+            console.log("Operation:", operation, "| action: dequeue");
+        } else if (operation == 3) {
+            console.log("Operation:", operation, "| action: print");
+        }
+    }
 } 
 
 processData('10\n1 42\n2\n1 14\n3\n1 28\n3\n1 60\n1 78\n2\n2');
