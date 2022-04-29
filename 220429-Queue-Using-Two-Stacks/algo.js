f4e53036ae2de5f74385d2de5be06b6888b895ed
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-29 11:36:47 -0700

    add first test case

--- 220429-Queue-Using-Two-Stacks/algo.js
+++ 220429-Queue-Using-Two-Stacks/algo.js
@@ -1,3 +1,10 @@
 ////////////////////////////////////////////
 // 220429 Queue Using Tswo Stacks algo.js
 ////////////////////////////////////////////
+
+function processData(input) {
+    //Enter your code here
+    console.log({input});
+} 
+
+processData('10\n1 42\n2\n1 14\n3\n1 28\n3\n1 60\n1 78\n2\n2');
