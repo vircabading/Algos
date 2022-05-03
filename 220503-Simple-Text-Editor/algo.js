b964b24944b818367e9a213e1dcdfa9e9b1003b3
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-05-03 11:13:53 -0700

    iterate through input

--- 220503-Simple-Text-Editor/algo.js
+++ 220503-Simple-Text-Editor/algo.js
@@ -5,10 +5,19 @@
 function processData(input) {
     //Enter your code here
     console.log({input});
+    
+    let operations = [];
+    let element = "";
+    for (char of input) {
+        console.log({char});
+        if (char == "\n" || char == " ") {
+            console.log("got an element");
+        }
+    }
 } 
 
 console.log("*****************************");
 console.log("Simple Text Editor Algorithm");
 console.log("*****************************\n\n");
 
-processData("");
+processData("8\n1 abc\n3 3\n2 3\n1 xy\n3 2\n4\n4\n3 1");
