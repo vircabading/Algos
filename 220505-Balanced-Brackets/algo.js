7282a7b6bf2702ffe0933dd00248218bffb638f0
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-05-05 10:53:16 -0700

    add isBalanced function

--- 220505-Balanced-Brackets/algo.js
+++ 220505-Balanced-Brackets/algo.js
@@ -1,3 +1,12 @@
 ////////////////////////////////////////////////
 //  220505 Balanced Brackets algo.js
 ///////////////////////////////////////////////
+
+/**
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function isBalanced(s) {
+    
+}
