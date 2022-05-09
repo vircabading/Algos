90d9670c1d16076d53c4d4732a615b6546a2c6fa
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-05-09 09:44:28 -0700

    add pseudocode

--- 220505-Balanced-Brackets/algo.js
+++ 220505-Balanced-Brackets/algo.js
@@ -8,9 +8,16 @@
  */
 
 function isBalanced(s) {
-    let output = true;
+    // Pseudo Code
+    // 1. Iterate through string S
+    // 2. For each opening bracket, add it to a stack
+    // 3. For each closing backet, check to see if the corresponding bracket is last on the stack
+    // 3.A  If True, remove top bracket on stack and keep iterating through string S
+    // 3.B  If False, return 'NO'
+    // 4.   If iterated through entire string and stack is empty, return YES
+    // 4.A  If iterated through entire string and stack is NOT empty, return NO
 
-    return output;
+    return 'YES';
 }
 
 console.log("************************");
