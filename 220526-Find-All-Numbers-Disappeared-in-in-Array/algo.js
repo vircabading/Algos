087b0c1e598c63eac1bb53cba2894d29bea6e93a
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-06-06 16:05:21 -0700

    add test 2

--- 220526-Find-All-Numbers-Disappeared-in-in-Array/algo.js
+++ 220526-Find-All-Numbers-Disappeared-in-in-Array/algo.js
@@ -13,28 +13,33 @@
     //     and note which values are in the expected numbers array
     // Return the value in the expected numbers hash map that were not found
 
-    console.log({nums});
-
     // Create hash map of expected numbers in array
     let expectedNums = new Map();
     for (let x=1; x<= nums.length; x++) {
         expectedNums.set(x, 1);
     }
-    console.log({expectedNums});
 
     // Iterate throught the nums array
     //     and note which values are in the expected numbers array
     for (let value of nums) {
-        console.log({value});
         if (expectedNums.has(value)) {
-            console.log("Found", value, " in expeted nums");
             expectedNums.delete(value);
         }
     }
-    return expectedNums;
+
+    // Return the value in the expected numbers hash map that were not found
+    let output = [];
+    const expectedNumsKeys = expectedNums.keys();
+    let currentKey = expectedNumsKeys.next().value;
+    while (currentKey) {
+        output.push(currentKey);
+        currentKey = expectedNumsKeys.next().value;
+    }
+    return output;
 };
 
 console.log("******************************");
-console.log("Find all nujmbers disappeared in array Algorithm\n");
+console.log("Find all numbers disappeared in array Algorithm\n");
 
 console.log("findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]):", findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
+console.log("findDisappearedNumbers([1, 1]):", findDisappearedNumbers([1, 1]));
