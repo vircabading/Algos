467cc2a58c7879d2578db2d3b532a2a750bf05c0
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-06-06 11:59:33 -0700

    create expectedc nuyms hash map

--- 220526-Find-All-Numbers-Disappeared-in-in-Array/algo.js
+++ 220526-Find-All-Numbers-Disappeared-in-in-Array/algo.js
@@ -8,14 +8,22 @@
  */
  var findDisappearedNumbers = function(nums) {
     // Pseudo Code
-
+    // Create hash map of expected numbers in array
+    // Iterate throught the nums array
+    //     and note which values are in the epected numbers array
+    // Return the value in the expected numbers hash map that were not found
 
     console.log({nums});
 
- 
+    // Create hash map of expected numbers in array
+    let expectedNums = new Map();
+    for (let x=1; x<= nums.length; x++) {
+        expectedNums.set(x, 1);
+    }
+    console.log({expectedNums});
 };
 
 console.log("******************************");
 console.log("Find all nujmbers disappeared in array Algorithm\n");
 
-console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
+console.log("findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]):", findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
