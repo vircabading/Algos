df78a7b54265d80b3d8f223f27264ecb299cf987
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-06-06 09:43:25 -0700

    add function finddisappeared numbers

--- 220526-Find-All-Numbers-Disappeared-in-in-Array/algo.js
+++ 220526-Find-All-Numbers-Disappeared-in-in-Array/algo.js
@@ -1,3 +1,11 @@
 ///////////////////////////////////////////////////////
 // Find ll Numbes Disappeared in Array Algorithm 220526
 ///////////////////////////////////////////////////////
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var findDisappearedNumbers = function(nums) {
+    console.log({nums});
+};
