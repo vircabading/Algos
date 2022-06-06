bf5c2136954f5f0fbf60c1693f25525d81026fc0
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-06-06 09:58:52 -0700

    call first test

--- 220526-Find-All-Numbers-Disappeared-in-in-Array/algo.js
+++ 220526-Find-All-Numbers-Disappeared-in-in-Array/algo.js
@@ -7,5 +7,12 @@
  * @return {number[]}
  */
  var findDisappearedNumbers = function(nums) {
+    
+
     console.log({nums});
 };
+
+console.log("******************************");
+console.log("Find all nujmbers disappeared in array Algorithm\n");
+
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
