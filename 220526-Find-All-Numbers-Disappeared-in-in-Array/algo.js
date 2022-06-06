e1f851b3e3c9713e07ac4ef438e25580f9871b1a
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-06-06 11:29:10 -0700

    restart algo

--- 220526-Find-All-Numbers-Disappeared-in-in-Array/algo.js
+++ 220526-Find-All-Numbers-Disappeared-in-in-Array/algo.js
@@ -8,31 +8,11 @@
  */
  var findDisappearedNumbers = function(nums) {
     // Pseudo Code
-    // 1. Find the expected array range
-    // 2. Recreate the expected array as a hash
-    // 3. Iterate throuigh the nums array and remove each item from the hash
-    // 4. Return any items left in the hash as an array of missing numbers
+
 
     console.log({nums});
 
-    // Find the Expected array range by finding largest number in nums array
-    let largestNum = 1;
-    for (let x of nums) {   // Retrieve each value in the array
-        if (x > largestNum) {
-            largestNum = x;
-        }
-    }
-
-    // Recreate the expected array as a hash map
-    const expectedNums = new Map();
-    for (let x= 1; x<= largestNum; x++) {
-        expectedNums.set(x, 1);
-    }
-
-    // Iterate through the nums array and remove each item from the hash
-    for (let value of nums) { 
-        console.log({value});
-    }
+ 
 };
 
 console.log("******************************");
