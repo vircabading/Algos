427fe5f932d77d1572554e2cd1ec506da1861285
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-06-10 12:03:53 -0700

    add pseudocode

--- 220610-Single-Number/algo.js
+++ 220610-Single-Number/algo.js
@@ -8,6 +8,12 @@
  */
  var singleNumber = (nums) => {
     // Pseudo Code
+    // Create an empty hash map
+    // Iterate through the nums array
+    // For each number in nums array
+    //      Add that number and count to the hash map
+    // Iterate through the hash map and return any number
+    //      that occured an odd number of times
     console.log({nums});
 
     return null;    // Nothing found
