6d8db8a66e59a9ef37a1df789caa400842f05d30
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-06-10 11:55:44 -0700

    add single number function

--- 220610-Single-Number/algo.js
+++ 220610-Single-Number/algo.js
@@ -1,3 +1,11 @@
 //////////////////////////////////////////////////
 // 220610 Single Number Algorithm algo.js
 //////////////////////////////////////////////////
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var singleNumber = (nums) => {
+    console.log({nums});
+};
