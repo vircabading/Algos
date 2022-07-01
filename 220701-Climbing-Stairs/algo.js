914bfae74321d41774ab38df92539e711dea62b2
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-07-01 14:03:24 -0700

    remove console logs

--- 220701-Climbing-Stairs/algo.js
+++ 220701-Climbing-Stairs/algo.js
@@ -12,21 +12,10 @@
     // add either a 1 or 0 to each possible answer
     // if the possible answer adds up to n, then increase the number of answers
     // after all posibilities tried return the number of answers
-
-    var getTotal = (arr) => {
-        let total = 0;
-        for (let x=0; x<arr.length; x++) {
-            total += arr[x];
-        }
-        return total;
-    }
-
     let numAns = 0;
     let posAns = [];
 
     for (let x=1; x<=n; x++) {
-        console.log({x});
-        console.log({posAns});
         let newPosAns = [];
 
         if (posAns.length == 0) {
@@ -41,9 +30,7 @@
                 newPosAns.push(2);
             }
         } else {
-            console.log("Found non empty posAns", posAns);
             for (curPosAns of posAns) {
-                console.log({curPosAns});
                 if ((curPosAns + 1) == n) {
                     numAns++;
                 } else {
