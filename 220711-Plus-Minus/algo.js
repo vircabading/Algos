e0a361396cb0f17d10173af386c1e82bfcf3443c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-07-11 07:14:00 -0700

    add plusMinus function to algo.js

--- 220711-Plus-Minus/algo.js
+++ 220711-Plus-Minus/algo.js
@@ -1,3 +1,12 @@
 ////////////////////////////////////////////////////
 // Plus Minus Algorithm
 ////////////////////////////////////////////////////
+
+/** */
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    // Write your code here
+    console.log({arr});
+}
