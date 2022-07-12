b7f5b3392916223ed0eeae630794b4a2f24fd836
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-07-12 11:03:37 -0700

    create a pseudocode section to algo.js

--- 220712-Min-Max-Sum/algo.js
+++ 220712-Min-Max-Sum/algo.js
@@ -7,10 +7,14 @@
  */
 
 function miniMaxSum(arr) {
+    // Pseudo Code
     
-
+    console.log({arr});
 }
 
 console.log("*****************************************");
 console.log("Min Max Algorithm");
 console.log("*****************************************");
+
+console.log("miniMaxSum([1, 3, 5, 7, 9])");
+miniMaxSum([1, 3, 5, 7, 9]);
