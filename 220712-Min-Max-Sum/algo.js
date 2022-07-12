014aa3e74494166fd9309fecc6eaa00ea093de4b
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-07-12 11:09:54 -0700

    add pseudocode

--- 220712-Min-Max-Sum/algo.js
+++ 220712-Min-Max-Sum/algo.js
@@ -8,7 +8,14 @@
 
 function miniMaxSum(arr) {
     // Pseudo Code
-    
+    // Iterate through the array
+    //   while iterating through the array
+    //      1. Find the smallest integer in the array
+    //      2. Find the largest integer in the array
+    //      3. Find the total of all the integers in array
+    // A. Smallest possible total = total - largest int in array
+    // B. Largest possible total = total - smallest int in array
+
     console.log({arr});
 }
 
