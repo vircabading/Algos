2c9eabe6ad7d288c90b0190a78a4e5d62af787cd
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-07-15 15:32:18 -0700

    add second test case to algo.js

--- 220715-Time-Conversion/algo.js
+++ 220715-Time-Conversion/algo.js
@@ -21,10 +21,20 @@ function timeConversion(s) {
     // Retrieve the hour
     let hour = parseInt(time[0] + time[1]);
     console.log({hour});
+    
+    // Convert the hour to the corrected hour
+    if (isPM && hour < 12) {
+        hour = hour + 12;
+    } else if (!isPM && hour === 12) {
+        hour = 0;
+    }
+
+    console.log("Corrected hour:",  hour);
 }
 
 console.log("*******************************");
 console.log("Time Conversion Algorithm");
 console.log("*******************************\n");
 
-console.log("timeConversion('07:05:45PM'):", timeConversion('07:05:45PM'));
+console.log("timeConversion('07:05:45PM'):", timeConversion('07:05:45PM'), "\n");
+console.log("timeConversion('12:01:00PM'):", timeConversion('12:01:00PM'), "\n");
