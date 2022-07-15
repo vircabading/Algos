ce6d9c359d1a29def48b8438bd131876f90d110b
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-07-15 14:49:23 -0700

    add timeConversion function to algo.js

--- 220715-Time-Conversion/algo.js
+++ 220715-Time-Conversion/algo.js
@@ -1,3 +1,13 @@
 /////////////////////////////////////////////////
 // 220715 Time Conversion Algo.js
 /////////////////////////////////////////////////
+
+/** */
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion(s) {
+    // Write your code here
+    console.log({s});
+}
