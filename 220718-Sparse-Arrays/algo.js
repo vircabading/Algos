495a573ec52ab3b1f7197170b5099d3f75dc22df
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-07-18 10:52:43 -0700

    add function matching strings

--- 220718-Sparse-Arrays/algo.js
+++ 220718-Sparse-Arrays/algo.js
@@ -2,3 +2,15 @@
 // 220718 Sparse Arrays Algo.js
 ///////////////////////////////////////////////////
 
+/**
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY strings
+ *  2. STRING_ARRAY queries
+ */
+
+function matchingStrings(strings, queries) {
+    // Write your code here
+    console.log({strings});
+    console.log({queries});
+}
