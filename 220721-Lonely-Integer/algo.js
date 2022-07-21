6009bad25feffa7f234c3af5d0fa777d9cad5c1e
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-07-21 10:00:49 -0700

    add lenelyInteger function in algo.js

--- 220721-Lonely-Integer/algo.js
+++ 220721-Lonely-Integer/algo.js
@@ -1,3 +1,13 @@
 /////////////////////////////////////////////////////
 // 220721 Lonely Integer Algo.js
 /////////////////////////////////////////////////////
+
+/**
+ * The function accepts INTEGER_ARRAY a as parameter.
+ * The function is expected to return an INTEGER.
+ */
+
+function lonelyinteger(a) {
+   
+
+}
