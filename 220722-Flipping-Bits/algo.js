7f91059cd7e3ab94c365ac56c0002a578d977a10
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-07-22 11:38:25 -0700

    add flipping bits algorithm

--- 220722-Flipping-Bits/algo.js
+++ 220722-Flipping-Bits/algo.js
@@ -1,3 +1,13 @@
 ///////////////////////////////////////////////////////
 // 220722 Flipping Bits Algo.js
 ///////////////////////////////////////////////////////
+
+/**
+ * The function accepts LONG_INTEGER n as parameter.
+ * The function is expected to return a LONG_INTEGER.
+ */
+
+function flippingBits(n) {
+    // Write your code here
+    console.log({n});
+}
