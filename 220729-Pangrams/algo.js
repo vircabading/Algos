7aaa353a707de0a1a72b44f4048454aeca79b53e
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-07-29 13:30:42 -0700

    return if pangram if lettersFound = 26

--- 220729-Pangrams/algo.js
+++ 220729-Pangrams/algo.js
@@ -16,6 +16,21 @@ function pangrams(s) {
     //          then add one to the letters found count
     // If the letters found count = 26, return "pangram"
     //     otherwise return "not pangram"
+    
+    // Create a map of all 26 letres initialized to value of zero
+    const letterMap = {
+        "a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0
+    }
+    let lettersFound = 0;
+
+    console.log({letterMap});
+
+    // If the letters found count = 26, return "pangram"
+    //     otherwise return "not pangram"
+    if (lettersFound >= 26) {
+        return "pangram";
+    }
+    return "not pangram";
 }
 
 console.log("***************************************");
