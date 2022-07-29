6ddeb5b591dd350b8dece13dee6d8ef0c1855920
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-07-28 22:19:54 -0700

    create couting sort algorithm

--- 220727-Counting-Sort-1/algo.js
+++ 220727-Counting-Sort-1/algo.js
@@ -1,3 +1,13 @@
 ///////////////////////////////////////////
 // 220728 Counting Sort Algo.js
 ///////////////////////////////////////////
+
+/**
+* Input Param: INTEGER_ARRAY arr
+* Returns: INTEGER_ARRAY.
+ */
+
+function countingSort(arr) {
+    // Write your code here
+    console.log({arr});
+}
