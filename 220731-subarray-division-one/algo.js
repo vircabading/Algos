bec950ecbefb318537757e0eb39b1b6a69118539
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-07-31 16:17:07 -0700

    create subarray division one algorithm algo.js

---
+++ 220731-subarray-division-one/algo.js
@@ -0,0 +1,30 @@
+///////////////////////////////////////////////
+// 220731 subarray division one algo.js
+///////////////////////////////////////////////
+
+/**
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY s
+ *  2. INTEGER d
+ *  3. INTEGER m
+ * The function is expected to return an INTEGER.
+ */
+
+function birthday(s, d, m) {
+    // Write your code here
+    console.log({s});
+    console.log({d});
+    console.log({m});
+}
+
+
+console.log("*************************************");
+console.log("Subarray Division One Algorithm");
+console.log("*************************************\n");
+
+const s1 = [2, 2, 1, 3, 2];
+const d1 = 4;
+const m1 = 2;
+
+console.log("S:", s1, "| Day:", d1, " | Month:", m1);
+console.log("Number of valid subarrays:", birthday(s1, d1, m1), "\n");
