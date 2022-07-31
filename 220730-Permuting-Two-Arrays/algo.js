2a5754bf4f9647f4004032f35c3781c58c58c201
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-07-30 17:04:44 -0700

    add function two arrays to algo.js

--- 220730-Permuting-Two-Arrays/algo.js
+++ 220730-Permuting-Two-Arrays/algo.js
@@ -2,6 +2,21 @@
 // 220730 Permuting Two Arrays algo.js
 /////////////////////////////////////////////////////
 
+/**
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY A
+ *  3. INTEGER_ARRAY B
+ * The function is expected to return a STRING.
+ */
+
+function twoArrays(k, A, B) {
+    // Write your code here
+    console.log({k});
+    console.log({A});
+    console.log({B});
+}
+
 console.log("***********************************");
 console.log("Permuting Two Arrays Algorithm");
 console.log("***********************************\n");
