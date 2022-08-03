c14274ad9d87a1964c9007f1ca45cae13224e116
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-03 10:13:33 -0700

    add sockMerfchant function to algo.js

--- 220803-sales-by-match/algo.js
+++ 220803-sales-by-match/algo.js
@@ -1,3 +1,16 @@
 ///////////////////////////////////////////
 // 220803 Sales by Match algo.js
 ///////////////////////////////////////////
+
+/**
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ * The function is expected to return an INTEGER.
+ */
+
+function sockMerchant(n, ar) {
+    console.log({n});
+    console.log({ar});
+    // Write your code here
+}
