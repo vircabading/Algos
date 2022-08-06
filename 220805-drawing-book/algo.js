07120f497abefd0f7d4f9178458618fdb6ba7c4a
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-06 00:11:51 -0700

    add pageCount function to algo.js

--- 220805-drawing-book/algo.js
+++ 220805-drawing-book/algo.js
@@ -1,3 +1,16 @@
 /////////////////////////////////////////
 // 220805 Drawing Book Algo.js
 /////////////////////////////////////////
+
+/**
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ * The function is expected to return an INTEGER.
+ */
+
+function pageCount(n, p) {
+    // Write your code here
+    console.log({n});
+    console.log({p});
+}
