8b7813bdf0b5d79563913dc8a358003a6d9a3809
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-08 15:33:51 -0700

    add towerBreakers function in algo.js

--- 220808-tower-breakers/algo.js
+++ 220808-tower-breakers/algo.js
@@ -1,3 +1,16 @@
 ///////////////////////////////////////////
 // 220808 Tower Breakers Algo.js
 ///////////////////////////////////////////
+
+/**
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER m
+ * The function is expected to return an INTEGER.
+ */
+
+function towerBreakers(n, m) {
+    console.log({n});
+    console.log({m});
+    // Write your code here
+}
