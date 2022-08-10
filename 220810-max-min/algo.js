5b32c6172f929ee67e90ece673764bb74c9d2c45
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-10 11:27:22 -0700

    add maxMin function to algo.js

--- 220810-max-min/algo.js
+++ 220810-max-min/algo.js
@@ -1,3 +1,18 @@
 ////////////////////////////////////////
 // 220810 Max Min Algorithm Read Me
 ////////////////////////////////////////
+
+/**
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY arr
+ * The function is expected to return an INTEGER.
+ */
+
+function maxMin(k, arr) {
+    console.log({k});
+    console.log({arr});
+    // Write your code here
+}
+
+maxMin(3, []);
