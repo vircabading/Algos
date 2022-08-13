cd2faf482a20392d00a4611b37c41cf147a86a13
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-13 07:27:35 -0700

    add ceasarCipher function to algo.js

--- 220813-ceasar-cipher/algo.js
+++ 220813-ceasar-cipher/algo.js
@@ -1,3 +1,16 @@
 //////////////////////////////////////////////
 // 220813 Ceaser Cipher algo.js
 //////////////////////////////////////////////
+
+/**
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. INTEGER k
+ * The function is expected to return a STRING.
+ */
+
+function caesarCipher(s, k) {
+    // Write your code here
+    console.log({s});
+    console.log({k});
+}
