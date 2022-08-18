45fe89e88b8e90743068fdc262eaef67d138e47c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-18 11:57:08 -0700

    add pseudocode to algo.js

--- 220817-grid-challenge/algo.js
+++ 220817-grid-challenge/algo.js
@@ -10,7 +10,12 @@
 function gridChallenge(grid) {
     console.log({grid});
     // Pseudo Code
-
+    // Create a two dimensional grid
+    //      To create this two 2D grid, must sort each strin in parameter passed grid
+    // Iterate through each column of the grid
+    //      To check if they are in alphabetical order
+    // If any portion is not in alphabetical order, return 'NO'
+    //      Otherwise, return 'YES'
 }
 
 console.log("*********************************");
