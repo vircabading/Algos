8e2b51533d45d40cf6f7986d9619ff1a0b79bb75
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-18 11:17:54 -0700

    add first test case to algo.js

--- 220817-grid-challenge/algo.js
+++ 220817-grid-challenge/algo.js
@@ -8,6 +8,7 @@
  */
 
 function gridChallenge(grid) {
+    console.log({grid});
     // Write your code here
 
 }
@@ -15,3 +16,8 @@ function gridChallenge(grid) {
 console.log("*********************************");
 console.log("Grid Challenge Algorithm");
 console.log("*********************************\n");
+
+const grid1 = ['abc', 'ade', 'efg'];
+
+console.log("*** Grid:", grid1, "***");
+console.log("Grid Challenge:", gridChallenge(grid1));
