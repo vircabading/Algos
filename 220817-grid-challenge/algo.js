397103b522f4a013c6c4c24b27006a79b11b7405
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-18 12:29:34 -0700

    alphabetize each row

--- 220817-grid-challenge/algo.js
+++ 220817-grid-challenge/algo.js
@@ -16,8 +16,18 @@ function gridChallenge(grid) {
     //      To check if they are in alphabetical order
     // If any portion is not in alphabetical order, return 'NO'
     //      Otherwise, return 'YES'
+    let twoDGrid = [];
 
     //      To create this two 2D grid, must sort each string in parameter passed grid
+    for (let word of grid) {
+        console.log({word});
+        twoDGrid.push(word.split(""));
+        twoDGrid[twoDGrid.length-1].sort((a,b) => 
+        {
+            return a.charCodeAt(0) - b.charCodeAt(0);
+        });
+    }
+    console.log({twoDGrid});
 
     // Otherwise, return 'YES'
     return 'YES';
@@ -27,7 +37,7 @@ console.log("*********************************");
 console.log("Grid Challenge Algorithm");
 console.log("*********************************\n");
 
-const grid1 = ['abc', 'ade', 'efg'];
+const grid1 = ['bac', 'eda', 'gef'];
 
 console.log("*** Grid:", grid1, "***");
 console.log("Grid Challenge:", gridChallenge(grid1));
