b3e19315a00354d79069bfbba6e0547c0c3f922f
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-25 10:12:50 -0700

    add pseudocode

--- 220825-sherlock-and-array/algo.js
+++ 220825-sherlock-and-array/algo.js
@@ -10,7 +10,21 @@
 function balancedSums(arr) {
     console.log({arr});
     // Pseudo Code
-
+    // Get the sum of the array
+    // Iterate through the array
+    //      while iterating array, get sum to left and right of current position
+    //          sum to left of current position: start at 0, add as pointer iterates
+    //          sum to right of current position: total - (current position's value + sum to left)
+    //      if find left sum and right sum are equal, return YES
+    // If iterated through array and couldn't find a balanced sum, return NO
+    
+    // Get the total sum of the array
+    let totalSum=0;
+    for (let num of arr) {
+        console.log({num});
+    }
+    
+    // If iterated through array and couldn't find a balanced sum, return NO
     return "NO";
 }
 
