7492f5b39ee903a28d0bacca70ddfb52359d6f28
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-25 09:50:00 -0700

    add balance sums function to algo.js

---
+++ 220825-sherlock-and-array/algo.js
@@ -0,0 +1,13 @@
+/////////////////////////////////////////////
+// 220825 Sherlock and Array
+/////////////////////////////////////////////
+
+/**
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ * The function is expected to return a STRING.
+ */
+
+function balancedSums(arr) {
+    // Write your code here
+
+}
