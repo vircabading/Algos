d7f3ce8ee391373e5ab4dad8fc57eb3b522393f4
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-29 11:17:10 -0700

    add superDigit function to algo.js

--- 220829-recursive-digit-sum/algo.js
+++ 220829-recursive-digit-sum/algo.js
@@ -2,7 +2,16 @@
 // 220829 recursive digit sum algo.js
 //////////////////////////////////////////////
 
+/**
+ * @param(string) n
+ * @param(integer) k
+ * @returns(integer)
+ */
 
+function superDigit(n, k) {
+    // Write your code here
+
+}
 
 console.log("******************************");
 console.log("Recursive Digit Sum");
