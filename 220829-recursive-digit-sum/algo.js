b71ef171ffb59fafc146aa7eaa66727afcd9d1c8
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-29 11:23:44 -0700

    add first test case

--- 220829-recursive-digit-sum/algo.js
+++ 220829-recursive-digit-sum/algo.js
@@ -16,3 +16,10 @@ function superDigit(n, k) {
 console.log("******************************");
 console.log("Recursive Digit Sum");
 console.log("******************************\n");
+
+const n1 = '9875';
+const k1 = 1;
+const expect1 = 2
+
+console.log("N:", n1, "| K:", k1, "| Expected:", expect1);
+console.log("Super Digit:", superDigit(n1, k1), "\n");
