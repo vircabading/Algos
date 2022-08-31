e1124230996fe8bf8194b7dfb6c004b49322d79d
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-31 11:33:13 -0700

    create counterGame function in algo.js

---
+++ 220831-counter-game/algo.js
@@ -0,0 +1,13 @@
+/////////////////////////////////////////////////
+// 220831 Counter Game algo.js
+/////////////////////////////////////////////////
+
+/**
+ * @param {LONG_INTEGER} LONG_INTEGER n
+ * @returns {String} STRING: winner, Louise or Richard.
+ */
+
+function counterGame(n) {
+    // Write your code here
+
+}
