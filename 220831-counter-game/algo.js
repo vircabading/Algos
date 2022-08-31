f12833c529a4d29e5ecd3557ed88d57082115847
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-08-31 11:50:56 -0700

    add pseudo code to algo.js

--- 220831-counter-game/algo.js
+++ 220831-counter-game/algo.js
@@ -9,7 +9,16 @@
 
 function counterGame(n) {
     console.log({n});
-    // Write your code here
+    // Pseudo Code
+    // Game Mechanic
+    //      if num is 1, game is overs
+    //      if num is NOT a power of 2
+    //          find the next lower number that is a power of 2
+    //          then subtract that number from num
+    //      if num IS a power of 2
+    //          divide num by 2
+    // Create a function which will play the game
+    //      and returns the number of steps needed to end the game
 
 }
 
