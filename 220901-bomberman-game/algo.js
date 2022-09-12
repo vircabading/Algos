fe6656ae911f953fa28266fa6f434b9cab4b0bd4
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-09-11 23:02:42 -0700

    add bomberMan function in algo.js

--- 220901-bomberman-game/algo.js
+++ 220901-bomberman-game/algo.js
@@ -1,3 +1,16 @@
 /////////////////////////////////////////////
 // 220901 Bombrerman Game algo.js
 /////////////////////////////////////////////
+
+/** */
+* The function accepts following parameters:
+*  1. INTEGER n
+*  2. STRING_ARRAY grid
+ * The function is expected to return a STRING_ARRAY.
+ */
+
+function bomberMan(n, grid) {
+    console.log({n});
+    console.log({grid});
+    // Write your code here
+}
