4197eac032d9a92e9f3352603c22bd5a3549357a
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-09-20 01:06:23 -0700

    create single number function in algo.js

--- 220920-Single-Number/algo.js
+++ 220920-Single-Number/algo.js
@@ -1,3 +1,12 @@
 //////////////////////////////////////////
 // 220920 Single Number algo.js
 //////////////////////////////////////////
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ const singleNumber = (nums) => {
+    
+};
+
