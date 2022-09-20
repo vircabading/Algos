5a930c32b7342986408b73dc8698d189fdb3bd98
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-09-20 01:11:01 -0700

    add first test case to algo.js

--- 220920-Single-Number/algo.js
+++ 220920-Single-Number/algo.js
@@ -13,3 +13,9 @@
 console.log("*****************************");
 console.log("Single Number Algorithm");
 console.log("*****************************\n");
+
+const nums1 = [2, 2, 1];
+const expected1 = 1;
+
+console.log("Nums:", nums1, "| expected output:", expected1);
+console.log("Single number:", singleNumber(nums1));
