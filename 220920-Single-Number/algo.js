3fdac09171d57f08d6f19d76ef8f35e8930370ec
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-09-23 10:19:33 -0700

    create pseudo code

--- 220920-Single-Number/algo.js
+++ 220920-Single-Number/algo.js
@@ -9,6 +9,13 @@
  const singleNumber = (nums) => {
     console.log({nums});
     // Pseudo Code
+    // Create a nums hash map
+    // For each number in nums
+    //      if the number is not in the hash map
+    //          intialize the numbers as a key with a value (count) as 1
+    //      if the number is in the hash map
+    //          increase the value (count) of that number's key by 1
+    // Return the key from the hash map with a value of 1
 };
 
 console.log("*****************************");
@@ -19,4 +26,4 @@ const nums1 = [2, 2, 1];
 const expected1 = 1;
 
 console.log("Nums:", nums1, "| expected output:", expected1);
-console.log("Single number:", singleNumber(nums1));
+console.log("Single number:", singleNumber(nums1), "\n");
