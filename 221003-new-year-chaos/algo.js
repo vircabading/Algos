b9de2b0d68b56678ab6ec498f3f32a9778c20310
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-10-05 10:04:38 -0700

    add title output to console log in algo.js

--- 221003-new-year-chaos/algo.js
+++ 221003-new-year-chaos/algo.js
@@ -2,7 +2,7 @@
 // 221003 Ne Year Chaos algo.js
 /////////////////////////////////////
 
-/** */
+/**s
  * The function accepts INTEGER_ARRAY q as parameter.
  */
 
@@ -10,3 +10,7 @@ function minimumBribes(q) {
     // Write your code here
     console.log({q});
 }
+
+console.log("*******************************");
+console.log("New Year Chaos Algorithm");
+console.log("*******************************\n");
