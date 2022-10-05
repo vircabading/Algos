940569c01ef5cac9d9c837d4d57fcedffd713e77
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-10-05 10:24:14 -0700

    iterate through the queue from the back

--- 221003-new-year-chaos/algo.js
+++ 221003-new-year-chaos/algo.js
@@ -10,13 +10,26 @@ function minimumBribes(q) {
     // Pseudo Code
     // Create place holder variables
     //      keep track of number of moves used
-    // Iterate through the que from the back
+    // Iterate through the queue from the back
     //      Note: the expected number is the index of the array + 1
     //      if the expected number is at the back
     //          remove the number at the back of the queue
     //          then move on to the next number at the back of the queue
-    //      
+    //      if the expected number is one away from the back of the queue
+    //          add one to the number of moves used
+    //          remove the expected number from where it's located in queue
+    //      if the expected number is two away from the back of the queue
+    //          add two to the number of moves used
+    //          remove the expected number from where it's located in queue
+    //      otherwise, then that means that the expected number
+    //          is more than two places away from its original location
+    //              therefore, 
+    //                  print "Too chaotic"
+    //                  exit the function
+    // After the queue has been reduce to a single digit
+    //      return the number of moves used
 }
+
 
 console.log("*******************************");
 console.log("New Year Chaos Algorithm");
