9088a75fbf7ba48ea052883db98ff8b75003418c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-10-14 12:56:30 -0700

    create isValid function in algo.js

--- 221014-sherlock-and-the-valid-string/algo.js
+++ 221014-sherlock-and-the-valid-string/algo.js
@@ -1,3 +1,14 @@
 //////////////////////////////////////////////////////
 // 221014 Sherlock and the Valid String algo.js
 //////////////////////////////////////////////////////
+
+/**
+ * The function accepts STRING s as parameter.
+ * The function is expected to return a STRING.
+ */
+
+function isValid(s) {
+    console.log({s});
+    // Write your code here
+
+}
