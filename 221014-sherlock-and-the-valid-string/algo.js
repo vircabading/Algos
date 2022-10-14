3f854e015534ccdb2bfcf7e6ef22ee5393e9cddb
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-10-14 13:00:41 -0700

    add first test case

--- 221014-sherlock-and-the-valid-string/algo.js
+++ 221014-sherlock-and-the-valid-string/algo.js
@@ -19,3 +19,6 @@ console.log("*******************************************\n");
 
 const s1 = 'abc';
 const expect1 = 'YES';
+
+console.log("String:", s1, "| Expected return:", expect1);
+console.log("Is Valid String:", isValid(s1), "\n");
