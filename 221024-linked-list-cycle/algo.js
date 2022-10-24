0f6cda61c70029dad2e02174ab69afb6a01d4fe0
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-10-24 10:13:09 -0700

    create hasCycle funtion in algo.js

--- 221024-linked-list-cycle/algo.js
+++ 221024-linked-list-cycle/algo.js
@@ -1,3 +1,20 @@
 //////////////////////////////////////////////
 // 221024 Linkecd List Cycle algo.js
 //////////////////////////////////////////////
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ let hasCycle = (head) => {
+    
+};
+
