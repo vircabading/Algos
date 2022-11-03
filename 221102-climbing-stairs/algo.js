d7f54df3dd69614e4c1497dbfa074eebef79b8de
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-11-02 22:50:24 -0700

    add first test case

--- 221102-climbing-stairs/algo.js
+++ 221102-climbing-stairs/algo.js
@@ -13,3 +13,9 @@
 console.log("***********************************");
 console.log("Climbing Stairs Algorithm");
 console.log("***********************************\n");
+
+const n1 = 2
+const exp1 = 2
+
+console.log("N:", n1, "| expected output:", exp1);
+console.log("Solution:", climbStairs(n1));
