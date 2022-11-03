06d3c0c65da5242e75ea553c3c401f8cbea4f4a5
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-11-02 22:58:12 -0700

    remove debug console logs

--- 221102-climbing-stairs/algo.js
+++ 221102-climbing-stairs/algo.js
@@ -7,16 +7,18 @@
  * @return {number}
  */
  var climbStairs = (n) => {
-    console.log({n});
     // base case
     if (n <= 2) {
         return n;
     }
     let one = 1;
     let two = 1;
-    for (let i=0; i <= n-1; i++) {
-        console.log({i});
+    for (let i=1; i <= n-1; i++) {
+        let temp = one;
+        one = one + two;
+        two = temp;
     }
+    return one;
 };
 
 console.log("***********************************");
