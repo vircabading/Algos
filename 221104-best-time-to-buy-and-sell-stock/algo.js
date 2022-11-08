b4a8d531dc00dea120e0cc3bf742a607926fd4c0
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-11-08 07:52:37 -0800

    add first test case to algo.js

--- 221104-best-time-to-buy-and-sell-stock/algo.js
+++ 221104-best-time-to-buy-and-sell-stock/algo.js
@@ -7,9 +7,15 @@
  * @return {number}
  */
  var maxProfit = function(prices) {
-    
+    console.log({prices})
 };
 
 console.log("***********************************");
 console.log("Best Time to Buy and Sell Stock");
 console.log("***********************************\n");
+
+const prices1 = [7, 1, 5, 3, 6, 4];
+const expOutput1 = 5;
+
+console.log("**** Prices:", prices1, "| Expected:", expOutput1, "********");
+console.log("Max profit:", maxProfit(prices1));
