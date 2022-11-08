f8b0b67de3fcd86163873acc7438797c6f95483f
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-11-08 07:46:18 -0800

    add maxProfit function to algo.js

--- 221104-best-time-to-buy-and-sell-stock/algo.js
+++ 221104-best-time-to-buy-and-sell-stock/algo.js
@@ -1,3 +1,11 @@
 //////////////////////////////////////////////////////
 // 221104 Best Time to Buy and Sell Stock algo.js
 //////////////////////////////////////////////////////
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit = function(prices) {
+    
+};
