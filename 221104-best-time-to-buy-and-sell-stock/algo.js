9a393a456d7b0bbacee432afb6584e2c652c3343
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-11-08 07:57:55 -0800

    add pseudocode to algo.js

--- 221104-best-time-to-buy-and-sell-stock/algo.js
+++ 221104-best-time-to-buy-and-sell-stock/algo.js
@@ -7,7 +7,18 @@
  * @return {number}
  */
  var maxProfit = function(prices) {
-    console.log({prices})
+    // Pseudo Code
+    // Initialize Variables
+    //      minVal = first price
+    //      maxProfit = zero
+    // Iterate through each price in prices
+    //      1. if the current price is less than minVal,
+    //          A. Current price becomes new minVal
+    //      2. get currentMaxProfit
+    //          A. which is: current price minus minVal
+    //      3. if currentMaxProfit is greater than maxProfit
+    //          A. currentMaxProfit becomes the maxProfit
+    // return the maxProfit
 };
 
 console.log("***********************************");
