5bd0f4ee00bf43f729085e73fdb79d1613c8ce9b
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-01-28 09:30:34 -0800

    create solve function in algo.js

--- 230128-30-days-of-code-day-2-operators/algo.js
+++ 230128-30-days-of-code-day-2-operators/algo.js
@@ -1,4 +1,18 @@
 // 30 days of code: day 2 - operations, algo.js
 
+/**
+ * Complete the 'solve' function below.
+ *
+ * The function accepts following parameters:
+ *  1. DOUBLE meal_cost
+ *  2. INTEGER tip_percent
+ *  3. INTEGER tax_percent
+ */
+
+function solve(meal_cost, tip_percent, tax_percent) {
+    // Write your code here
+
+}
+
 console.log("*****************************");
 console.log("30 days of code: Day 2 Operations\n");
