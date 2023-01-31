2960425aa9b8744720886d84c7ddd1c2792a88de
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-01-31 09:34:03 -0800

    add first test case, call isWeird function, pass N parameter into isWeird function

--- 230130-30-days-of-code-day-3-intro-to-condition-statements/main.js
+++ 230130-30-days-of-code-day-3-intro-to-condition-statements/main.js
@@ -1,8 +1,12 @@
 // 30 Days of Code: Day 3 - Into to Condition Statements
 
 const isWeird = (N) => {
-    
+    console.log(N);
 }
 
 console.log("*******************************");
 console.log("30 Days of Code: (Day 3) Intro to condition statements\n");
+
+console.log("*******************************");
+console.log("Number: 3");
+isWeird(3);
