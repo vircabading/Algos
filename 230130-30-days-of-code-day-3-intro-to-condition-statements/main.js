6338e7f752e889d62d9dc96cb5b40af1ac0ec720
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-01-31 09:30:11 -0800

    get title terminal output to main.js

---
+++ 230130-30-days-of-code-day-3-intro-to-condition-statements/main.js
@@ -0,0 +1,8 @@
+// 30 Days of Code: Day 3 - Into to Condition Statements
+
+const isWeird = (N) => {
+    
+}
+
+console.log("*******************************");
+console.log("30 Days of Code: (Day 3) Intro to condition statements\n");
