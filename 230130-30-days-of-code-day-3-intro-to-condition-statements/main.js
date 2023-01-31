76b42193d309f529c4015c864092cfc48fbc539d
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-01-31 09:41:32 -0800

    implement conditional statements to determine if N is weird or not

--- 230130-30-days-of-code-day-3-intro-to-condition-statements/main.js
+++ 230130-30-days-of-code-day-3-intro-to-condition-statements/main.js
@@ -2,8 +2,20 @@
 
 const isWeird = (N) => {
     // If N is odd, print Weird
-    // if N is odd and is in the range of 6 to 20, print Weird
-    // Otherwise, print Not Weird
+    if (N % 2 == 1) {
+        console.log("Weird");
+    }
+    else {
+        // if N is odd and is in the range of 6 to 20, print Weird
+        if ((N>=6) && (N<=20)) {
+            console.log("Weird");
+        }
+
+        // Otherwise, print Not Weird
+        else {
+            console.log("Not Weird");
+        }
+    }
 }
 
 console.log("*******************************");
