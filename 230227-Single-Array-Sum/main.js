57bdf4609a5923b32bf3e0122074d265a62dde7b
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-02-27 10:05:28 -0800

    implement simple array sum function

--- 230227-Single-Array-Sum/main.js
+++ 230227-Single-Array-Sum/main.js
@@ -12,12 +12,18 @@ function simpleArraySum(ar) {
     // 2. Add each array in the integer into a sum
     // 3. Return the sum
 
-    
+    let sum = 0;
+    for (let i=0; i< ar.length; i++) {
+        sum += ar[i];
+    }
+    return sum;
 }
 
 console.log("***************************");
 console.log("Single Array Sum Algorithm");
 console.log("***************************\n");
 
-let arrayOfInts = [1,2,3,4,10,11]
-simpleArraySum(arrayOfInts);
+let arrayOfInts = [1,2,3,4,10,11];
+
+console.log("*** Array of Integers:", arrayOfInts, "***");
+console.log("Sum:", simpleArraySum(arrayOfInts));
