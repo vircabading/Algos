93e4b6dfcbae34d1a7d657ba2f352fa913c48e6b
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-02-27 09:25:59 -0800

    add title termina;l output to main.js

--- 230227-Single-Array-Sum/main.js
+++ 230227-Single-Array-Sum/main.js
@@ -1,3 +1,7 @@
 ///////////////////////////////////////
 // 230227 Single Array Sum Algorithm main.js
 ///////////////////////////////////////
+
+console.log("***************************");
+console.log("Single Array Sum Algorithm");
+console.log("***************************\n");
