9e85475961a176c7cba163ff89e106767032edf6
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-02-27 09:33:19 -0800

    create and call single array sum function

--- 230227-Single-Array-Sum/main.js
+++ 230227-Single-Array-Sum/main.js
@@ -2,6 +2,18 @@
 // 230227 Single Array Sum Algorithm main.js
 ///////////////////////////////////////
 
+/**
+ * *** Simple Array Sum the finds the sum of an array of integers ***
+ * @param {Array of integers} ar 
+ */
+function simpleArraySum(ar) {
+    console.log({ar});
+
+}
+
 console.log("***************************");
 console.log("Single Array Sum Algorithm");
 console.log("***************************\n");
+
+let arrayOfInts = [1,2,3,4,10,11]
+simpleArraySum(arrayOfInts);
