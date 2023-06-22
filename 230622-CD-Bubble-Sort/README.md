0d6414b0bbf8182bbb06227aa8496ff5c59540f6
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-06-22 10:53:48 -0500

    implement bubbleSort() function

--- 230622-CD-Bubble-Sort/README.md
+++ 230622-CD-Bubble-Sort/README.md
@@ -1,1 +1,24 @@
-# 
+# Bubble Sort Read Me
+
+## Description
+
+    https://www.hackerearth.com/practice/algorithms/sorting/bubble-sort/visualize/
+
+    Stable sort
+    Time Complexity
+        - Best: O(n) linear when array is already sorted.
+        - Average: O(n^2) quadratic.
+        - Worst: O(n^2) quadratic when the array is reverse sorted.
+    Space: O(1) constant.
+
+    For review, create a function that uses BubbleSort to sort an unsorted array in-place.
+    For every pair of adjacent indices, swap them if the item on the left is larger than the item on the right until array is fully sorted
+    -> Assume there are no duplicates
+
+## Language
+
+JavaScript
+
+## Origin
+
+Coding Dojo > MERN Stack
--- 230622-CD-Bubble-Sort/main.js
+++ 230622-CD-Bubble-Sort/main.js
@@ -1,15 +1,6 @@
 // bubbleSort()
 /* 
-    https://www.hackerearth.com/practice/algorithms/sorting/bubble-sort/visualize/
-    Stable sort
-    Time Complexity
-        - Best: O(n) linear when array is already sorted.
-        - Average: O(n^2) quadratic.
-        - Worst: O(n^2) quadratic when the array is reverse sorted.
-    Space: O(1) constant.
-    For review, create a function that uses BubbleSort to sort an unsorted array in-place.
-    For every pair of adjacent indices, swap them if the item on the left is larger than the item on the right until array is fully sorted
-    -> Assume there are no duplicates
+
 */
 
 const nums1 = [5, 3, 4, 2, 1];
@@ -17,14 +8,26 @@ const nums2 = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
 const nums3 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 function bubbleSort(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length - 1 - i; j++) {
-            if (nums[j] > nums[j + 1]) {
-                [nums[j], nums[j+1]] = [nums[j+1], nums[j]];
+    let sortedNums = nums;
+
+    // 1. Decrement max from end of array until max is equal to zero
+    // 2. start at front and move until end of array swapping left and righ elements until last element is highest element
+
+    let endIndex = sortedNums.length - 1;
+    while (endIndex > 0) {
+        for (leftIdx = 0; leftIdx < endIndex; leftIdx++) {
+            const rightIdx = leftIdx + 1;
+            if ( sortedNums[leftIdx] > sortedNums[rightIdx] ) {
+                // swap left and right
+                const tempNum = sortedNums[rightIdx];
+                sortedNums[rightIdx] = sortedNums[leftIdx];
+                sortedNums[leftIdx] = tempNum;
             }
         }
+        endIndex--;
     }
-    return nums;
+
+    return sortedNums;
 }
 
 console.log("******************************************");
